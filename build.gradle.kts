a0a917462e2925cc01fd6ab9d1483e720787a937
yoneltt <66741756+yoneltt@users.noreply.github.com>
2025-09-27 00:59:26 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,32 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.droid4linux.app"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.droid4linux.app"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+    buildTypes {
+        release { isMinifyEnabled = false }
+        debug { isDebuggable = true }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("org.nanohttpd:nanohttpd:2.3.1")
 }
